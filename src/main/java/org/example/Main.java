ef2238049059ceaedb93690b3d663c7da5f2b0f1
Gabrik65 <g.valenzuela10@ufromail.cl>
2025-04-25 10:59:41 -0400

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        menu();
+    }
+
+
+    public static void menu() {
+        String option = sc.next();
+        if(isInt(sc.next())){
+            ejecutarOpcion(stringtoint(option));
+        }else {
+            System.out.println("Numero no valido");
+            menu();
+        }
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("Menu Principal");
+        System.out.println("1- Suma");
+        System.out.println("6- Salir");
+        System.out.println("Selecciona una opcion:");
+    }
+
+    public static void ejecutarOpcion(int opcion) {
+        switch(opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+               break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                System.exit(69);
+                break;
+            default:
+                System.out.println("Opcion invalida");
+                menu();
+        }
+    }
+
+    public static int stringtoint(String number){
+        int num = 0;
+        if(isInt(number)){
+        num = Integer.parseInt(number);
+        }else {
+            System.out.println("Numero Invalido");
+        }
+        return num;
+    }
+
+    public static boolean isInt(String number){
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
