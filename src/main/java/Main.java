168c629621d40980c21defc595af0091b2e8414e
MountainKing187 <g.vergara05@ufromail.cl>
2025-04-26 16:37:17 -0400

    Eliminados dependencias sin usar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,4 @@
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
